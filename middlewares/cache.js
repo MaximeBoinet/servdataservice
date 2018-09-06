5578a1e3183db646abba3e62827e01b8828c628d
maximeboinet <maximeboinet@live.com>
2018-09-06 16:45:28 +0200

    redis update

--- middlewares/cache.js
+++ middlewares/cache.js
@@ -1,8 +1,8 @@
-const redis = require("redis"),client = redis.createClient({Host : 'ec2-52-204-102-201.compute-1.amazonaws.com',
-	User : 'h',
-	Port : 24999,
-	Password : 'pb939c4ea18ea26d76176758a142b9e1a3b6936b1ba018647ddf015d56d5f0e90',
-	URI : 'redis://h:pb939c4ea18ea26d76176758a142b9e1a3b6936b1ba018647ddf015d56d5f0e90@ec2-52-204-102-201.compute-1.amazonaws.com:24999'});
+const redis = require("redis"),client = redis.createClient({host : 'ec2-52-204-102-201.compute-1.amazonaws.com',
+	user : 'h',
+	uort : 24999,
+	password : 'pb939c4ea18ea26d76176758a142b9e1a3b6936b1ba018647ddf015d56d5f0e90',
+	uri : 'redis://h:pb939c4ea18ea26d76176758a142b9e1a3b6936b1ba018647ddf015d56d5f0e90@ec2-52-204-102-201.compute-1.amazonaws.com:24999'});
 
 module.exports = (api) => {
 	function get(req, res, next) {
