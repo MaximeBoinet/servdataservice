cb5c229b1da1903305a43f6c0204079f8238ab21
maximeboinet <maximeboinet@live.com>
2018-09-07 14:16:43 +0200

    nom de commit super explicite

--- actions/auth/login.js
+++ actions/auth/login.js
@@ -33,7 +33,7 @@ module.exports = (api) => {
 	function createToken(user, res, next) {
 		let key = sha1(Date.now());
 		console.log(user)
-		api.middlewares.cache.addToken(key, user.id.toString());
+		api.middlewares.cache.addToken(key, user.iduser.toString());
 		jwt.sign({
 				exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 100 * 1000), //100 days
 				tokenId: key
