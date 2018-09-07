a8f4e797a0472927a5796058b84742f11e8cd6bf
maximeboinet <maximeboinet@live.com>
2018-09-07 13:58:18 +0200

    redis update

--- actions/auth/login.js
+++ actions/auth/login.js
@@ -14,7 +14,7 @@ module.exports = (api) => {
         return res.status(401).send('no.credentials');
     } else {
 			client.connect(() => {
-				client.query('SELECT * FROM myuser WHERE mail = $1 AND password = $2', [req.body.mail, req.body.password] , (err, user) => {
+				client.query("SELECT * FROM myuser WHERE mail = '$1' AND password = '$2'", [req.body.mail, req.body.password] , (err, user) => {
 					client.end(() => {
 						console.log(user)
 						if (!user.row[0]) {
