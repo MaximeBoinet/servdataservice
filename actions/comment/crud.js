6044afc173a04ccbade1d97d5bfae1afed2926c9
Utilisateur <maximeboinet@live.com>
2018-09-26 12:06:57 +0200

    patch comments

--- actions/comment/crud.js
+++ actions/comment/crud.js
@@ -4,25 +4,26 @@ const PORT = process.env.PORT || 5000;
 const { DATABASE_URL } = process.env;
 
 module.exports = (api) => {
-  function postComment (res, req, next) {
+
+  function postComment (req, res, next) {
     const client = new Client({
       connectionString: DATABASE_URL,
     });
     client.connect()
       .then(() => client.query('INSERT INTO comment(mark, review, user_iduser) VALUES ($1, $2, $3) RETURNING *', [req.body.mark, req.body.review, req.body.iduser]))
-      .then(resp => res.send(resp.rows[0]))
-      .catch(e => res.send(e) )
+      .then((resp) => res.send(resp.rows[0]))
+      .catch((e) => res.send(e))
       .then(() => client.end())
   }
 
-  function  getComments (res, req, next) {
+  function  getComments (req, res, next) {
     const client = new Client({
       connectionString: DATABASE_URL,
     });
     client.connect()
       .then(() => client.query('SELECT * FROM comment WHERE user_iduser = $1', [req.params.iduser]))
-      .then(resp => res.send(resp.rows))
-      .catch(e => res.send(e))
+      .then((resp) => res.send(resp.rows))
+      .catch((e) => res.send(e))
       .then(() => client.end())
   }
 
